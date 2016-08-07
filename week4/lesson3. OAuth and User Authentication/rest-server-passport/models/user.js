9d425f991c933d715090e23cc825f8700f539136
babjo <kd980311@naver.com>
2016-08-07 16:40:04 +0900

    week4 학습중

---
+++ week4/lesson3. OAuth and User Authentication/rest-server-passport/models/user.js
@@ -0,0 +1,28 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var passportLocalMongoose = require('passport-local-mongoose');
+
+var User = new Schema({
+    username: String,
+    password: String,
+    firstname : {
+        type : String,
+        default : ''
+    },
+    lastname :{
+        type: String,
+        default : ''
+    },
+    admin:{
+        type:Boolean,
+        default:false
+    }
+});
+
+User.plugin(passportLocalMongoose);
+
+User.methods.getName =function(){
+    return (this.firstname + ' ' + this.lastname);
+}
+
+module.exports = mongoose.model('User', User);
